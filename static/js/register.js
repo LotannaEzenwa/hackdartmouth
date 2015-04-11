d973837c5be4c85807b382132e246b5710b59377
Byrne Hollander <bhollander823@gmail.com>
2015-04-11 15:00:31 -0400

    Register

--- static/js/register.js
+++ static/js/register.js
@@ -3,25 +3,25 @@ $(function() {
     $(".button").click(function() {
       // validate and process form here
       
-      $('.error').hide();
-  	  var email = $("input#email").val();
-  		if (email == "" || email.length() < 4) {
-        $("label#email_error").show();
-        $("input#email").focus();
-        return false;
-      }
-  		var username = $("input#username").val();
-  		if (username == "" || username.length() < 3) {
-        $("label#username_error").show();
-        $("input#username").focus();
-        return false;
-      }
-  		var password = $("input#password").val();
-  		if (password == "" || password.length() < 6) {
-        $("label#password_error").show();
-        $("input#password").focus();
-        return false;
-      }
+    //   $('.error').hide();
+  	 //  var email = $("input#email").val();
+  		// if (email == "" || email.length() < 4) {
+    //     $("label#email_error").show();
+    //     $("input#email").focus();
+    //     return false;
+    //   }
+  		// var username = $("input#username").val();
+  		// if (username == "" || username.length() < 3) {
+    //     $("label#username_error").show();
+    //     $("input#username").focus();
+    //     return false;
+    //   }
+  		// var password = $("input#password").val();
+  		// if (password == "" || password.length() < 6) {
+    //     $("label#password_error").show();
+    //     $("input#password").focus();
+    //     return false;
+    //   }
       var dataString = 'email='+ email + '&username=' + username + '&password=' + password;
 	  //alert (dataString);return false;
 	  $.ajax({
